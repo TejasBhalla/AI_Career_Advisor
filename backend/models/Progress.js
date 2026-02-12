7f89a6583d138367737488791ea7bfed500db6cf
Tejas Bhalla <51715020+TejasBhalla@users.noreply.github.com>
2026-02-12 18:52:04 +0530

    updated progress model

--- backend/models/Progress.js
+++ backend/models/Progress.js
@@ -4,7 +4,7 @@ const skillSchema = new mongoose.Schema({
   name: { type: String, required: true },
   completed: { type: Boolean, default: false },
   month: { type: Number} 
-});
+}); 
 
 const progressSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
